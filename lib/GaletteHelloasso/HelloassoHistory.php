9eb561347e0a3f5e29e8d75dbf86ab3523ac05c6
Guillaume AGNIERAY <dev@agnieray.net>
2026-08-11 22:23:28 +0200

    Display member full name in the list

--- lib/GaletteHelloasso/HelloassoHistory.php
+++ lib/GaletteHelloasso/HelloassoHistory.php
@@ -16,6 +16,7 @@ use Galette\Core\Galette;
 use Galette\Core\Login;
 use Galette\Core\History;
 use Galette\Core\Preferences;
+use Galette\Entity\Adherent;
 use Galette\Filters\HistoryList;
 
 /**
@@ -132,6 +133,9 @@ class HelloassoHistory extends History
                         $oa = Galette::jsonDecode($o['request']);
                     }
 
+                    $member_id = $oa['metadata']['member_id'] ?? 0;
+
+                    $o['member_fullname'] = $this->getMemberFullName($member_id);
                     $o['raw_request'] = print_r($oa, true);
                     $o['request'] = $oa;
 
@@ -146,6 +150,29 @@ class HelloassoHistory extends History
             }
         }
         return $new;
+    }
+
+    /**
+     * Gets Member full name
+     *
+     * @param int $id ID of the member to retrieve
+     * @return string
+     */
+    protected function getMemberFullName(int $id): string
+    {
+        $fullname = _T('None', 'helloasso');
+
+        $select = $this->zdb->select(Adherent::TABLE);
+        $select->columns(['prenom_adh', 'nom_adh']);
+        $select->where(['id_adh' => $id]);
+        $result = $this->zdb->execute($select);
+        $row = $result->current();
+
+        if ($row) {
+            $fullname = mb_strtoupper($row['nom_adh']) . ' ' . $row['prenom_adh'];
+        }
+
+        return $fullname;
     }
 
     /**
