437b5ac0b5c161ecaf8c339d2b16e126a6faecfb
Guillaume AGNIERAY <dev@agnieray.net>
2026-08-14 13:52:57 +0200

    Update public donations already existing in DB
    Update the `state` value from 0 to 3.
    
    Existing records in the plugin's history with `state` 0 are the only ones that
    are supposed to be public donations. Public donations now use value 3. This
    value already existed and was associated with "incomplete" payments, but in
    pratice it was never used because only succesful payments were processed. So it
    is safe to simply update records with `state` 0 to 3.

---
+++ scripts/upgrade-to-1.1.0-pgsql.sql
@@ -0,0 +1,7 @@
+--
+-- This file is part of Galette Helloasso plugin (https://galette-community.github.io/plugin-helloasso).
+-- SPDX-FileCopyrightText: Copyright © 2025-2026 The Galette Team
+-- SPDX-License-Identifier: GPL-3.0-or-later
+--
+
+UPDATE galette_helloasso_history SET state = 3 WHERE state = 0;
