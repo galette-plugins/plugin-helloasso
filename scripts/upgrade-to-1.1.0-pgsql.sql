03c929bc9487a1b2e27144dfad1c6c83759ff1cc
Guillaume AGNIERAY <dev@agnieray.net>
2026-08-17 15:57:13 +0200

    Store relevant JSON values in the history table

--- scripts/upgrade-to-1.1.0-pgsql.sql
+++ scripts/upgrade-to-1.1.0-pgsql.sql
@@ -4,5 +4,28 @@
 -- SPDX-License-Identifier: GPL-3.0-or-later
 --
 
-UPDATE galette_helloasso_history SET state = 3 WHERE state = 0;
 INSERT INTO galette_helloasso_preferences (nom_pref, val_pref) VALUES ('helloasso_sepa_option', '');
+
+ALTER TABLE galette_helloasso_history
+  ADD COLUMN payer_name character varying(255) NOT NULL,
+  ADD COLUMN member_id integer NOT NULL,
+  ADD COLUMN method character varying(10) NOT NULL,
+  ADD COLUMN receipt_url character varying(255) NOT NULL;
+
+UPDATE galette_helloasso_history
+SET
+  state = CASE
+    WHEN state = 0 THEN 3
+    ELSE state
+  END,
+  payer_name = CONCAT(
+    UPPER((request->'data'->'payer'->>'lastName')),
+    ' ',
+    (request->'data'->'payer'->>'firstName')
+  ),
+  member_id = COALESCE(
+    (request->'metadata'->>'member_id')::int,
+    0
+  ),
+  method = request->'data'->>'paymentMeans',
+  receipt_url = request->'data'->>'paymentReceiptUrl';
